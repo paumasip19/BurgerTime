0707cff2181e5460ee95edb0a0e9161b500caf83
Pau Masip <36194293+paumasip19@users.noreply.github.com>
2019-11-14 17:22:51 +0100

    Update
    - Added Menu

---
+++ BurgerTime/js/menu.js
@@ -0,0 +1,18 @@
+var burgertime = burgertime || {};
+
+burgertime.menu ={
+    init:function(){
+    
+    },
+    preload:function(){
+        var ruta = 'assets/sprites/';
+        
+        this.load.image('bg', ruta+'menu_background.png');
+    },
+    create:function(){
+      
+    },
+    update:function(){
+      
+    }
+};
