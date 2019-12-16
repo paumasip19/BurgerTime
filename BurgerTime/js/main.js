61058467e91596c9d20ba986cecd148a1b5e48f7
Pau Masip <36194293+paumasip19@users.noreply.github.com>
2019-12-16 11:57:49 +0100

    Little Update

--- BurgerTime/js/main.js
+++ BurgerTime/js/main.js
@@ -22,4 +22,4 @@ burgertime.game.state.add('level3',burgertime.level3);
 burgertime.game.state.add('ranking',burgertime.ranking);
 burgertime.game.state.add('menu',burgertime.menu);
 burgertime.game.state.add('access_screen',burgertime.access_screen);
-burgertime.game.state.start('level1');
+burgertime.game.state.start('access_screen');
